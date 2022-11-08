0e90c952fb7cbb3a1b2ceb6bd6713b50b9bfcc59
duongndagile <duongnd.agiletech@gmail.com>
2022-11-08 17:25:30 +0700

    add ionicons to tab navigator

--- App.tsx
+++ App.tsx
@@ -10,18 +10,45 @@
 
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Category from './src/screens/Category';
-import Home from './src/screens/Home';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import Category from './src/screens/Category';
+// import Home from './src/screens/Home';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Stack = createNativeStackNavigator();
+import Cart from './src/screens/Cart';
+import Orders from './src/screens/Orders';
+import Settings from './src/screens/Settings';
+import Categories from './src/screens/Categories';
+
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Store Application Logo" component={Home} />
-        <Stack.Screen name="Category" component={Category} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}: any) => ({
+          tabBarIcon: ({focused, color, size}: any) => {
+            let iconName = '';
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Cart') {
+              iconName = focused ? 'cart' : 'cart-outline';
+            } else if (route.name === 'Orders') {
+              iconName = focused ? 'reader' : 'reader-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={Categories} />
+        <Tab.Screen name="Cart" component={Cart} />
+        <Tab.Screen name="Orders" component={Orders} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Cart from './Cart';
 import Orders from './Orders';
@@ -10,7 +10,19 @@ import Categories from './Categories';
 const Tab = createBottomTabNavigator();
 const Home = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({router}: any) => ({
+        tabBarIcon: ({focused, color, size}: any) => {
+          let iconName;
+          if (router.name === 'Home') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (router.name === 'Cart') {
+            iconName = focused ? 'ios-cart' : 'ios-cart-outline';
+            console.log(iconName);
+          }
+          return <Ionicons name="ios-home" size={size} color={color} />;
+        },
+      })}>
       <Tab.Screen name="Home" component={Categories} />
       <Tab.Screen name="Cart" component={Cart} />
       <Tab.Screen name="Orders" component={Orders} />
